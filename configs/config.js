f66ba437cd76c18e65c10809a33d6973cc58b54a
xuke8 <xuke8@xuke8deMac-mini.local>
2019-07-30 21:00:52 +0800

    feat update color

--- configs/config.js
+++ configs/config.js
@@ -1,36 +1,65 @@
 import { StyleSheet } from 'react-native';
 import { scale, scaleIgnoreSysScale } from '../scale/index';
 
+const sizes = {
+  huge: 36,
+  xxxLarger: 24,
+  xxLarge: 20,
+  xLarge: 18,
+  large: 16,
+  normal: 14,
+  small: 12,
+  tiny: 10,
+};
+
+const colors = {
+  title: "rgba(0,0,0,0.85)",
+  primary: "rgba(0,0,0,0.65)",
+  secondary: "rgba(0,0,0,0.45)",
+  dark1: "rgba(0,0,0,0.25)",
+  dark2: "rgba(0,0,0,0.15)",
+  dark3: "rgba(0,0,0,0.09)",
+  white: "rgb(0,0,0)",
+  grey1: "rgba(255,255,255,0.85)",
+  grey2: "rgba(255,255,255,0.65)",
+  grey3: "rgba(255,255,255,0.45)",
+  grey4: "rgba(255,255,255,0.25)",
+  grey5: "rgba(255,255,255,0.15)",
+  grey6: "rgba(255,255,255,0.09)",
+  success: "#52c41a",
+  warning: "#faad14",
+  error: "#ff190c",
+};
 
 const styles = StyleSheet.create({
   title: {
     fontSize: 24,
-    color: colors.dark.title,
+    color: colors.title,
     lineHeight: 32
   },
   secondaryTitle: {
     fontSize: 20,
-    color: colors.dark.title,
+    color: colors.title,
     lineHeight: 28
   },
   tertiaryTitle: {
     fontSize: 16,
-    color: colors.dark.title,
+    color: colors.title,
     lineHeight: 24
   },
   primaryText: {
     fontSize: 14,
-    color: colors.dark.primary,
+    color: colors.primary,
     lineHeight: 22
   },
   secondaryText: {
     fontSize: 12,
-    color: colors.dark.secondary,
+    color: colors.secondary,
     lineHeight: 20
   },
   captionText: {
     fontSize: 10,
-    color: colors.dark.caption,
+    color: colors.dark3,
     lineHeight: 18
   }
 });
@@ -38,57 +67,25 @@ const styles = StyleSheet.create({
 const config = {
   scale,
   scalableItems: ['fontSize', 'lineHeight'],
-  sizes: {
-    huge: 36,
-    xxxLarger: 24,
-    xxLarge: 20,
-    xLarge: 18,
-    large: 16,
-    normal: 14,
-    small: 12,
-    tiny: 10,
-  },
-  colors: {
-    title: "rgba(0,0,0,0.85)",
-    primary: "rgba(0,0,0,0.65)",
-    secondary: "rgba(0,0,0,0.45)",
-    disabled: "rgba(0,0,0,0.25)",
-    border: "rgba(0,0,0,0.15)",
-    dividers: "rgba(0,0,0,0.09)",
-    white: "rgb(0,0,0)",
-    grey1: "rgba(255,255,255,0.85)",
-    grey2: "rgba(255,255,255,0.65)",
-    grey3: "rgba(255,255,255,0.45)",
-    grey4: "rgba(255,255,255,0.25)",
-    grey5: "rgba(255,255,255,0.15)",
-    grey6: "rgba(255,255,255,0.09)",
-    success: "#52c41a",
-    warning: "#faad14",
-    error: "#ff190c",
-  },
+  sizes,
+  colors,
   categories: {
     PrimaryTitle: {
-      props: {scalableItems: ['fontSize', 'lineHeight']},
       style: styles.title
     },
     SecondaryTitle: {
-      props: {},
       style: styles.secondaryTitle
     },
     TertiaryTitle: {
-      props: {},
       style: styles.tertiaryTitle
     },
     PrimaryText: {
-      props: {},
       style: styles.primaryText
     },
     SecondaryText: {
-      props: {},
       style: styles.secondaryText
     },
     CaptionText: {
-      props: {},
       style: styles.captionText
     }
   }
